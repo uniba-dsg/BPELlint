94fad46f66adeed52dd4407626ffd68b57672e38
Simon Harrer <simon.harrer@gmail.com>
2012-10-18 16:10:12 +0200

    Simplification of ErrorMessages.

--- src/main/java/de/uniba/wiai/dsg/ss12/isabel/io/ErrorMessages.java
+++ src/main/java/de/uniba/wiai/dsg/ss12/isabel/io/ErrorMessages.java
@@ -2,54 +2,40 @@ package de.uniba.wiai.dsg.ss12.isabel.io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class ErrorMessages {
 
-	public String getShort(String saNumber, int type)
-			throws DescriptionNotFoundException {
-		try (Scanner scanner = new Scanner(getFilePath(saNumber))) {
-			return getShortByType(type, scanner);
+    public String getShort(String saNumber, int type)
+            throws DescriptionNotFoundException {
+        try {
+            String content = getDescriptionFileContent(saNumber);
+            return content.split("\n")[type - 1].trim();
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            throw new DescriptionNotFoundException(
+                    "Short description for rule " + saNumber + " could not be found.");
+        }
+    }
 
-		} catch (FileNotFoundException e) {
-			throw new DescriptionNotFoundException(
-					"Expected File does not exist.");
-		}
-	}
+    public String getLong(String saNumber) throws DescriptionNotFoundException {
+        try {
+            String content = getDescriptionFileContent(saNumber);
+            return content.split("@@@")[1].trim();
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            throw new DescriptionNotFoundException(
+                    "Short description " + saNumber + " could not be found.");
+        }
+    }
 
-	private File getFilePath(String saNumber) {
-		return new File("Testcases/" + saNumber + "/" + saNumber + ".txt");
-	}
+    private String getDescriptionFileContent(String saNumber) throws IOException {
+        return new String(Files.readAllBytes(getFilePath(saNumber).toPath()));
+    }
 
-	private String getShortByType(int type, Scanner scanner)
-			throws DescriptionNotFoundException {
-		for (int i = 1; scanner.hasNextLine(); i++) {
-			String nextLine = scanner.nextLine();
-			if (i == type)
-				return nextLine;
-		}
+    private File getFilePath(String saNumber) {
+        return new File("Testcases/" + saNumber + "/" + saNumber + ".txt");
+    }
 
-		throw new DescriptionNotFoundException("Expected Short does not exist.");
-	}
-
-	public String getLong(String saNumber) throws DescriptionNotFoundException {
-		try (Scanner scanner = new Scanner(getFilePath(saNumber))) {
-			return skipLinesUntilLong(scanner);
-
-		} catch (FileNotFoundException e) {
-			throw new DescriptionNotFoundException(
-					"Expected File does not exist.");
-		}
-	}
-
-	private String skipLinesUntilLong(Scanner scanner)
-			throws DescriptionNotFoundException {
-		while (scanner.hasNextLine()) {
-			String nextLine = scanner.nextLine();
-			if ("@@@".equals(nextLine) && scanner.hasNextLine())
-				return scanner.nextLine();
-		}
-
-		throw new DescriptionNotFoundException("Expected Long does not exist.");
-	}
 }
