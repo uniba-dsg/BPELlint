6d90a3396692f317062d5a471e52997e3593a3f5
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2012-10-18 15:49:54 +0200

    implemented errorMessages into printer

--- src/main/java/de/uniba/wiai/dsg/ss12/isabel/io/ValidationResultPrinter.java
+++ src/main/java/de/uniba/wiai/dsg/ss12/isabel/io/ValidationResultPrinter.java
@@ -1,69 +1,30 @@
 package de.uniba.wiai.dsg.ss12.isabel.io;
 
-import de.uniba.wiai.dsg.ss12.isabel.tool.ValidationException;
+import java.util.List;
+
 import de.uniba.wiai.dsg.ss12.isabel.tool.ValidationResult;
 import de.uniba.wiai.dsg.ss12.isabel.tool.Violation;
-import nu.xom.*;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
 
 public class ValidationResultPrinter {
 
-	private Document errorDocument = null;
-	private final XPathContext MSG_CONTEXT = new XPathContext("err",
-			"http://lspi.wiai.uni-bamberg.de/ed-12-ss-proj-bpelval/schema/errormessages");
+	private ErrorMessages errorMessages;
 
 	public ValidationResultPrinter() {
-		try {
-			errorDocument = loadErrorMessages();
-		} catch (ValidationException e) {
-			System.out.println("Warning: Could not load static-analysis-violation-descriptions.");
-		}
+		errorMessages = new ErrorMessages();
 	}
 
-	public void printResults(VerbosityLevel verbosityLevel, ValidationResult violationCollector) {
+	public void printResults(VerbosityLevel verbosityLevel,
+			ValidationResult violationCollector) {
 		switch (verbosityLevel) {
-			case NORMAL:
-				printResults(violationCollector);
-				break;
-			case VERBOSE:
-				printResultsVerbose(violationCollector);
-				break;
-			case FULL:
-				printResultsFull(violationCollector);
-				break;
-		}
-	}
-
-	private Document loadErrorMessages() throws ValidationException {
-		Builder parser = new Builder();
-		try {
-
-			InputStream errorMessageStream = ValidationResultPrinter.class.getResourceAsStream("/errormessages.xml");
-			if (errorMessageStream == null) {
-				throw new ValidationException("Unable to load errormessages.xml, "
-						+ "the file was not found");
-			}
-
-			try (InputStreamReader reader = new InputStreamReader(errorMessageStream)) {
-				return parser.build(reader);
-			}
-		} catch (ValidityException e) {
-			throw new ValidationException(e, "Unable to load errormessages.xml, "
-					+ "this file is invalid");
-		} catch (ParsingException e) {
-			throw new ValidationException(e, "Unable to load errormessages.xml, "
-					+ "this file could not be parsed");
-		} catch (FileNotFoundException e) {
-			throw new ValidationException(e, "Unable to load errormessages.xml, "
-					+ "the file was not found");
-		} catch (IOException e) {
-			throw new ValidationException(e, "Unable to load errormessages.xml, "
-					+ "the reading unit is damaged");
+		case NORMAL:
+			printResults(violationCollector);
+			break;
+		case VERBOSE:
+			printResultsVerbose(violationCollector);
+			break;
+		case FULL:
+			printResultsFull(violationCollector);
+			break;
 		}
 	}
 
@@ -83,9 +44,6 @@ public class ValidationResultPrinter {
 	private void printOutputHeader(List<Violation> violations) {
 		if (!violations.isEmpty()) {
 			System.out.println("VIOLATIONS:\n");
-			if (errorDocument == null) {
-				printErrorDocumentFatal();
-			}
 		}
 	}
 
@@ -122,46 +80,33 @@ public class ValidationResultPrinter {
 		}
 	}
 
-	private void printLineRowSA(Violation violation, String saNumber, String previousSourceFile) {
+	private void printLineRowSA(Violation violation, String saNumber,
+			String previousSourceFile) {
 		if (!violation.fileName.equals(previousSourceFile))
 			System.out.println(violation.fileName + ":");
 
-		System.out.println("\tLine " + violation.row + ", Column " + violation.column + ", "
-				+ saNumber);
+		System.out.println("\tLine " + violation.row + ", Column "
+				+ violation.column + ", " + saNumber);
 	}
 
 	private void printShortDescription(String SANumber, int type) {
-		if (errorDocument == null) {
-			return;
-		}
-		String shortDescription;
-		Nodes nodes = errorDocument.query("//err:rule[@name='" + SANumber
-				+ "']/err:shorts/err:type[err:number=" + type + "]/err:content", MSG_CONTEXT);
 		try {
-			shortDescription = nodes.get(0).getValue().trim();
-		} catch (IndexOutOfBoundsException e) {
-			shortDescription = "error message not found (" + SANumber + ")";
+			System.out.println("\t\t" + errorMessages.getShort(SANumber, type));
+		} catch (DescriptionNotFoundException e) {
+			printErrorDocumentFatal(e.getMessage());
 		}
-		System.out.println("\t\t" + shortDescription);
 	}
 
 	private void printLongDescription(String SANumber) {
-		if (errorDocument == null) {
-			return;
-		}
-		String longDescription;
-		Nodes nodes = errorDocument.query("//err:rule[@name='" + SANumber + "']/err:long",
-				MSG_CONTEXT);
 		try {
-			longDescription = nodes.get(0).getValue().trim();
-		} catch (IndexOutOfBoundsException e) {
-			longDescription = "long description not found (" + SANumber + ")";
+			System.out.println("\t\t" + errorMessages.getLong(SANumber));
+		} catch (DescriptionNotFoundException e) {
+			printErrorDocumentFatal(e.getMessage());
 		}
-		System.out.println("\t\t" + longDescription);
 	}
 
-	private void printErrorDocumentFatal() {
-		System.out.println("Warning: Error message document is not specified.");
+	private void printErrorDocumentFatal(String error) {
+		System.out.println("Warning: " + error);
 	}
 
 }
