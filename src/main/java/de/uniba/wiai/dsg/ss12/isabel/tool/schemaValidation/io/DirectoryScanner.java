150b9db1faadcd43eb782e6dbee3bfd2a5bd807b
Simon Harrer <simon.harrer@gmail.com>
2012-10-19 13:51:27 +0200

    Simpler DirectoryScanner.

--- src/main/java/de/uniba/wiai/dsg/ss12/isabel/tool/schemaValidation/io/DirectoryScanner.java
+++ src/main/java/de/uniba/wiai/dsg/ss12/isabel/tool/schemaValidation/io/DirectoryScanner.java
@@ -1,36 +1,30 @@
 package de.uniba.wiai.dsg.ss12.isabel.tool.schemaValidation.io;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DirectoryScanner {
 
-    private final File dir;
     private final String fileEnding;
-    private List<File> files = new ArrayList<>();
 
-    public DirectoryScanner(File dir, String fileEnding) {
-        this.dir = dir;
+    public DirectoryScanner(String fileEnding) {
         this.fileEnding = fileEnding;
     }
 
-    public List<File> scan() {
-        files = new ArrayList<>();
+    public List<File> scanDir(File dir) {
+        List<File> result = new LinkedList<>();
 
-        scanDir(dir);
-        return files;
-    }
-
-    private void scanDir(File dir) {
         // add all corresponding files within the current folder
-        Collections.addAll(files, dir.listFiles(new FileFilenameFilter(fileEnding)));
+        Collections.addAll(result, dir.listFiles(new FileFilenameFilter(fileEnding)));
 
         // search for subdirectories
         for (File file : dir.listFiles(new DirFilenameFilter())) {
-            scanDir(file);
+            result.addAll(scanDir(file));
         }
+
+        return result;
     }
 
 }
