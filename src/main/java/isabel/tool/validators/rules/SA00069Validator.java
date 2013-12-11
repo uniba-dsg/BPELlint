746f38f17332e7afe18a83c5e8aae4a85b9d0971
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2013-12-11 13:14:19 +0100

    add SA00069
    see SA00064 is similar

---
+++ src/main/java/isabel/tool/validators/rules/SA00069Validator.java
@@ -0,0 +1,48 @@
+package isabel.tool.validators.rules;
+
+import isabel.tool.helper.NodeHelper;
+import isabel.tool.impl.Standards;
+import isabel.tool.impl.ValidationCollector;
+import isabel.tool.imports.ProcessContainer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import nu.xom.Node;
+import nu.xom.Nodes;
+
+public class SA00069Validator extends Validator {
+
+	public SA00069Validator(ProcessContainer files,
+			ValidationCollector validationCollector) {
+		super(files, validationCollector);
+	}
+
+	@Override
+	public void validate() {
+		Nodes targetsNodes = this.fileHandler.getBpel().getDocument()
+				.query("//bpel:targets", Standards.CONTEXT);
+		for (Node targets : targetsNodes) {
+			checkLinkNameUniqueness(targets);
+		}
+	}
+
+	private void checkLinkNameUniqueness(Node targets) {
+		Set<String> uniqueNames = new HashSet<>();
+		for (Node target : targets.query("./bpel:target", Standards.CONTEXT)) {
+			String name = new NodeHelper(target).getAttribute("linkName");
+
+			if (uniqueNames.contains(name)) {
+				addViolation(target);
+			} else {
+				uniqueNames.add(name);
+			}
+		}
+	}
+
+	@Override
+	public int getSaNumber() {
+		return 69;
+	}
+
+}
