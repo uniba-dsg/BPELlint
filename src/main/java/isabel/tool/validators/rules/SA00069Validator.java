778c42ce89c25f3d62ab59e258490f7ebd473bbf
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2013-12-12 16:38:44 +0100

    use model in SA00069

--- src/main/java/isabel/tool/validators/rules/SA00069Validator.java
+++ src/main/java/isabel/tool/validators/rules/SA00069Validator.java
@@ -1,8 +1,9 @@
 package isabel.tool.validators.rules;
 
-import isabel.model.NodeHelper;
 import isabel.model.ProcessContainer;
 import isabel.model.Standards;
+import isabel.model.bpel.TargetElement;
+import isabel.model.bpel.TargetsElement;
 import isabel.tool.impl.ValidationCollector;
 
 import java.util.HashSet;
@@ -23,14 +24,14 @@ public class SA00069Validator extends Validator {
 		Nodes targetsNodes = this.fileHandler.getBpel().getDocument()
 				.query("//bpel:targets", Standards.CONTEXT);
 		for (Node targets : targetsNodes) {
-			checkLinkNameUniqueness(targets);
+			checkLinkNameUniqueness(new TargetsElement(targets));
 		}
 	}
 
-	private void checkLinkNameUniqueness(Node targets) {
+	private void checkLinkNameUniqueness(TargetsElement targets) {
 		Set<String> uniqueNames = new HashSet<>();
-		for (Node target : targets.query("./bpel:target", Standards.CONTEXT)) {
-			String name = new NodeHelper(target).getAttribute("linkName");
+		for (TargetElement target : targets.getAllTargets()) {
+			String name = target.getLinkName();
 
 			if (uniqueNames.contains(name)) {
 				addViolation(target);
