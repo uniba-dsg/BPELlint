aaaa6f12006215322ef992bb4ee9b5255f1cfbdb
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2013-12-16 13:58:42 +0100

    add SA00058 tests

--- src/main/java/isabel/model/wsdl/MessageElement.java
+++ src/main/java/isabel/model/wsdl/MessageElement.java
@@ -21,31 +21,6 @@ public class MessageElement implements Referable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((message == null) ? 0 : message.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MessageElement other = (MessageElement) obj;
-		if (message == null) {
-			if (other.message != null)
-				return false;
-		} else if (!message.equals(other.message))
-			return false;
-		return true;
-	}
-
-	@Override
 	public Node toXOM() {
 		return message.toXOM();
 	}
