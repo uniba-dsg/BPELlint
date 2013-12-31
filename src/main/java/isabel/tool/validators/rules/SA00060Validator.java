d51c9cef420855b4458269fc90a0be084495c12e
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2013-12-31 14:29:27 +0100

    continue SA00060
    implementation seems to reach an dead end

--- src/main/java/isabel/tool/validators/rules/SA00060Validator.java
+++ src/main/java/isabel/tool/validators/rules/SA00060Validator.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import nu.xom.Node;
 import nu.xom.Nodes;
 
+import isabel.model.ComparableNode;
 import isabel.model.NavigationException;
+import isabel.model.NodeHelper;
 import isabel.model.ProcessContainer;
 import isabel.model.Standards;
 import isabel.model.bpel.mex.MessageActivity;
@@ -23,10 +25,50 @@ public class SA00060Validator extends Validator {
 
 	@Override
 	public void validate() {
+		List<OnEventElement> onEvents = fileHandler.getAllOnEvents();
+		for (OnEventElement onEvent : onEvents) {
+			try {
+				validate(onEvent);
+			} catch (NavigationException e) {
+				// ignore element without operation
+			}
+		}
 		Map<MessageActivity, Boolean> imas = new HashMap<>();
-		imas.putAll(getImas(fileHandler.getAllOnEvents()));
 		imas.putAll(getImas(fileHandler.getAllReceives()));
 		imas.putAll(getImas(fileHandler.getAllOnMessages()));
+		for (MessageActivity inboundMessageActivity : imas.keySet()) {
+			Boolean isClosed = !imas.get(inboundMessageActivity);
+			if (isClosed) {
+				continue;
+			}
+			for (MessageActivity similarIma : imas.keySet()) {
+				if (isSimilarOperation(inboundMessageActivity, similarIma)) {
+					Nodes replies = new NodeHelper(inboundMessageActivity.toXOM()).getEnclosingScope().toXOM().query(
+							".//bpel:reply[@partnerLink='"
+									+ inboundMessageActivity.getPartnerLinkAttribute()
+									+ "'][@operation='"
+									+ inboundMessageActivity.getOperationAttribute() + "']",
+							Standards.CONTEXT);
+				}
+			}
+		}
+	}
+
+	private boolean isSimilarOperation(MessageActivity ima, MessageActivity similarIma) {
+		boolean sameActivityIsNotSimilar = new ComparableNode(ima.toXOM())
+				.equals(new ComparableNode(similarIma.toXOM()));
+		if (sameActivityIsNotSimilar) {
+			return false;
+		}
+		try {
+			boolean samePortType = new ComparableNode(ima.getPortType().toXOM())
+					.equals(new ComparableNode(similarIma.getPortType().toXOM()));
+			boolean sameOperation = new ComparableNode(ima.getOperation().toXOM())
+					.equals(new ComparableNode(similarIma.getOperation().toXOM()));
+			return samePortType && sameOperation;
+		} catch (NavigationException e) {
+			return false;
+		}
 	}
 
 	private <E extends MessageActivity> Map<MessageActivity, Boolean> getImas(
@@ -35,12 +77,7 @@ public class SA00060Validator extends Validator {
 		for (MessageActivity messageActivity : messageActivities) {
 			try {
 				if (messageActivity.getOperation().isRequestResponse()) {
-					boolean isOpen = true;
-					if (messageActivity instanceof OnEventElement) {
-						isOpen = false;
-						validate((OnEventElement) messageActivity);
-					}
-					imas.put(messageActivity, isOpen);
+					imas.put(messageActivity, true);
 				}
 			} catch (NavigationException e) {
 				// ignore element without operation
@@ -50,8 +87,9 @@ public class SA00060Validator extends Validator {
 	}
 
 	private void validate(OnEventElement onEvent) throws NavigationException {
-		String correspondingReply = ".//bpel:reply[@portType='" + onEvent.getPortTypeAttribute()
-				+ "']" + "[@operation='" + onEvent.getOperationAttribute() + "']";
+		String correspondingReply = ".//bpel:reply[@partnerLink='"
+				+ onEvent.getPartnerLinkAttribute() + "']" + "[@operation='"
+				+ onEvent.getOperationAttribute() + "']";
 		Nodes replies = onEvent.toXOM().query(correspondingReply, Standards.CONTEXT);
 		if (isOperationUsedUniquelyInOnEventEnclosingScope(onEvent, replies.size())) {
 			// ignore if there are no replies, this is checked elsewhere
@@ -63,18 +101,20 @@ public class SA00060Validator extends Validator {
 		addViolation(onEvent);
 	}
 
-	private boolean isMarked(OnEventElement onEvent, Node node) {
-		String messageExchangeKey = onEvent.getMessageExchangeAttribute();
-		return new ReplyElement(node, fileHandler).getMessageExchangeAttribute()
-						.equals(messageExchangeKey) && !messageExchangeKey.isEmpty();
-	}
-
 	private boolean isOperationUsedUniquelyInOnEventEnclosingScope(OnEventElement onEvent,
 			int repliesInOnEvent) {
-		String allOperationMembersInScope = ".//bpel:*[@portType='"
-				+ onEvent.getPortTypeAttribute() + "']" + "[@operation='"
+		String allOperationMembersInScope = ".//bpel:*[@partnerLink='"
+				+ onEvent.getPartnerLinkAttribute() + "']" + "[@operation='"
 				+ onEvent.getOperationAttribute() + "']";
-		return onEvent.getEnclosingScope().toXOM().query(allOperationMembersInScope, Standards.CONTEXT).size() == 1 + repliesInOnEvent;
+		return onEvent.getEnclosingScope().toXOM()
+				.query(allOperationMembersInScope, Standards.CONTEXT).size() == 1 + repliesInOnEvent;
+	}
+
+	private boolean isMarked(OnEventElement onEvent, Node node) {
+		String messageExchangeKey = onEvent.getMessageExchangeAttribute();
+		return new ReplyElement(node, fileHandler).getMessageExchangeAttribute().equals(
+				messageExchangeKey)
+				&& !messageExchangeKey.isEmpty();
 	}
 
 	@Override
