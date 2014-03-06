918b8d285a8c3f1fc6da46851b718ecdc4ecb693
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2014-03-06 23:53:03 +0100

    implement getProperties
    not using identifiers any more

--- src/main/java/isabel/model/bpel/CorrelationSetElement.java
+++ src/main/java/isabel/model/bpel/CorrelationSetElement.java
@@ -5,9 +5,10 @@ import isabel.model.NavigationException;
 import isabel.model.NodeHelper;
 import isabel.model.PrefixHelper;
 import isabel.model.ProcessContainer;
+import isabel.model.wsdl.PropertyElement;
 import nu.xom.Node;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CorrelationSetElement extends ContainerAwareReferable {
@@ -19,19 +20,17 @@ public class CorrelationSetElement extends ContainerAwareReferable {
         this.correlationSet = new NodeHelper(correlationSet, "correlationSet");
     }
 
-    public List<String> getPropertyIdentifiers() {
-        List<String> result = new ArrayList<>();
+	public List<PropertyElement> getProperties() throws NavigationException {
+        List<PropertyElement> result = new LinkedList<>();
 
         // is a List of QNames which can only be accessed as a String
         String properties = correlationSet.getAttribute("properties");
         for (String property : properties.split(" ")) {
-            // resolving prefix via element
-            String prefix = PrefixHelper.getPrefix(property);
-            String namespace = correlationSet.asElement().getNamespaceURI(prefix);
+            String targetNamespace = PrefixHelper.resolveQNameToNamespace(correlationSet.toXOM(), property);
 
-            String propertyValueWithoutPrefix = PrefixHelper.removePrefix(property);
-
-            result.add("{" + namespace + "}" + propertyValueWithoutPrefix);
+            String name = PrefixHelper.removePrefix(property);
+            Node propertyNode = getProcessContainer().resolveName(targetNamespace, name, "vprop:property");
+            result.add(new PropertyElement(propertyNode, getProcessContainer()));
         }
 
         return result;
