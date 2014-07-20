687f4bdd86f7e3a12eb7c4d9b4c1d7024d6d2fa9
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2014-07-20 17:05:05 +0200

    remoce space characters

--- src/main/java/bpellint/model/bpel/BPELFaults.java
+++ src/main/java/bpellint/model/bpel/BPELFaults.java
@@ -5,10 +5,10 @@ public class BPELFaults {
 	public static final String[] VALUES = {"bpel:ambiguousReceive",
 			"bpel:completionConditionFailure", "bpel:conflictingReceive",
 			"bpel:conflictingRequest", "bpel:correlationViolation",
-			"bpel:invalidBranchCondition ", "bpel:invalidExpressionValue",
+			"bpel:invalidBranchCondition", "bpel:invalidExpressionValue",
 			"bpel:invalidVariables", "bpel:mismatchedAssignmentFailure",
 			"bpel:missingReply", "bpel:missingRequest",
-			"bpel:scopeInitializationFailure", "bpel:selectionFailure ",
+			"bpel:scopeInitializationFailure", "bpel:selectionFailure",
 			"bpel:subLanguageExecutionFault", "bpel:uninitializedPartnerRole",
 			"bpel:uninitializedVariable", "bpel:unsupportedReference",
 			"bpel:xsltInvalidSource", "bpel:xsltStylesheetNotFound"};
