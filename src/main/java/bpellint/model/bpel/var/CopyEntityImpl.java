6e396d301a29a906263bb37f847528342792d1df
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2014-08-23 13:48:03 +0200

    add partial sa32 fix

--- src/main/java/bpellint/model/bpel/var/CopyEntityImpl.java
+++ src/main/java/bpellint/model/bpel/var/CopyEntityImpl.java
@@ -20,7 +20,7 @@ public class CopyEntityImpl extends ContainerAwareReferable implements CopyEntit
         this.fromTo = new NodeHelper(node);
         this.partnerLinkDelegate = new PartnerLinkedImpl(this, getProcessContainer(), getPartnerLinkAttribute());
         amountOfAttributes = fromTo.getAmountOfAttributes();
-        if (fromTo.toXOM().query("./bpel:documentation", Standards.CONTEXT).hasAny()){
+        if (fromTo.toXOM().query("./bpel:documentation", Standards.CONTEXT).hasAny()) {
             amountOfRealChildren = fromTo.getAmountOfChildren() - 1;
         } else {
             amountOfRealChildren = fromTo.getAmountOfChildren();
@@ -29,18 +29,18 @@ public class CopyEntityImpl extends ContainerAwareReferable implements CopyEntit
 
     @Override
     public boolean isEmpty() {
-        return fromTo.hasNoChildren() && fromTo.hasNoAttributes() && fromTo.hasNoContent();
+        return fromTo.hasNoChildren() && fromTo.hasNoAttributes() && !fromTo.hasContent();
     }
 
     @Override
     public boolean isMessageVariableAssignment() {
+        if (fromTo.hasContent()){
+            return false;
+        }
         if (!fromTo.hasAttribute("variable")) {
             return false;
         }
-        if (amountOfAttributes > 2) {
-            return false;
-        }
-        if (amountOfRealChildren > 1) {
+        if (amountOfAttributes > 2 || amountOfRealChildren > 1) {
             return false;
         }
         if (amountOfRealChildren == 1) {
@@ -62,12 +62,14 @@ public class CopyEntityImpl extends ContainerAwareReferable implements CopyEntit
             }
         }
 
-        return amountOfAttributes == 1
-                || fromTo.hasAttribute("part");
+        return amountOfAttributes == 1 || fromTo.hasAttribute("part");
     }
 
     @Override
     public boolean isPartnerLinkAssignment() {
+        if (fromTo.hasContent()){
+            return false;
+        }
         if ("from".equals(fromTo.getLocalName())) {
             if (amountOfAttributes != 2) {
                 return false;
@@ -87,6 +89,9 @@ public class CopyEntityImpl extends ContainerAwareReferable implements CopyEntit
 
     @Override
     public boolean isVariableAssignment() {
+        if (fromTo.hasContent()){
+            return false;
+        }
         if (!fromTo.hasAttribute("variable")) {
             return false;
         }
@@ -109,6 +114,9 @@ public class CopyEntityImpl extends ContainerAwareReferable implements CopyEntit
 
     @Override
     public boolean isLiteralAssignment() {
+        if (fromTo.hasContent()){
+            return false;
+        }
         if (!"from".equals(fromTo.getLocalName())) {
             return false;
         }
