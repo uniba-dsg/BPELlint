7d310d27b59625a3444721ea634df6c2bb750390
Christian Preißinger <christian-roland.preissinger@stud.uni-bamberg.de>
2014-08-23 16:56:28 +0200

    simplify performance test

--- src/test/java/benchmark/PerformanceTest.java
+++ src/test/java/benchmark/PerformanceTest.java
@@ -1,62 +1,29 @@
 package benchmark;
 
-
-import bpellint.SAViolationTestData;
 import bpellint.io.EnvironmentVariableInterpreter;
-import bpellint.io.ValidationResultPrinter;
-import bpellint.io.VerbosityLevel;
 import bpellint.tool.BpelLint;
-import bpellint.tool.validators.result.ValidationResult;
 import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 import org.junit.*;
 import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.pmw.tinylog.Logger;
+import org.pmw.tinylog.LoggingLevel;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Paths;
-import java.util.*;
 
-import static org.junit.Assert.assertEquals;
 
 /**
  * Executes tests based on the files from within the /Testcases directory.
  */
-@RunWith(value = Parameterized.class)
+
 public class PerformanceTest {
+
     @Rule
     public TestRule benchmark = new BenchmarkRule();
 
-	protected final Set<Integer> violatedRules;
-    protected final String bpel;
-
-	public PerformanceTest(String bpel, String violatedRules) {
-		this.violatedRules = parseString(violatedRules);
-		this.bpel = bpel;
-	}
-
-	private Set<Integer> parseString(String violatedRules) {
-		String[] elements = violatedRules.split(",");
-		Set<Integer> parsedElements = new HashSet<>();
-		for (String element : elements) {
-			if (element == null || element.isEmpty()) {
-				continue;
-			}
-			parsedElements.add(Integer.parseInt(element.trim()));
-		}
-		return parsedElements;
-	}
-
-	@Parameterized.Parameters(name = "{index}: {0} violates {1}")
-	public static Collection<Object[]> data() throws IOException {
-		return PerformanceTestData.saViolationTests();
-	}
-
     @Before
     public void setUp() throws Exception {
+        Logger.setLoggingLevel(LoggingLevel.OFF);
         System.setProperty(EnvironmentVariableInterpreter.BPEL_LINT_SA_RULES_ENVIRONMENT_VARIABLE, "ALL");
     }
 
@@ -67,17 +34,26 @@ public class PerformanceTest {
 
     @BenchmarkOptions(benchmarkRounds = 100, warmupRounds = 10)
     @Test
-	public void testValidators() throws Exception {
-		ValidationResult validationResult = new BpelLint().validate(Paths.get(bpel));
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		new ValidationResultPrinter(ps).printResults(VerbosityLevel.NORMAL,
-				validationResult);
-		String data = "\n" + baos.toString() + "\n";
-
-		assertEquals("BPEL: " + bpel + data, violatedRules,
-				validationResult.getViolatedRules());
+	public void measureSimpleProcess() throws Exception {
+		new BpelLint().validate(Paths.get("Testcases/betsy/structured/Sequence.bpel"));
 	}
+
+    @BenchmarkOptions(benchmarkRounds = 100, warmupRounds = 10)
+    @Test
+    public void measureMediumProcess() throws Exception {
+        new BpelLint().validate(Paths.get("Testcases/betsy/structured/Flow-GraphExample.bpel"));
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 100, warmupRounds = 10)
+    @Test
+    public void measureSimpleErroneousProcess() throws Exception {
+        new BpelLint().validate(Paths.get("Testcases/rules/SA00001/Notification.bpel"));
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 100, warmupRounds = 10)
+    @Test
+    public void measureComplexProcess() throws Exception {
+        new BpelLint().validate(Paths.get("Testcases/complex/bpel_req/Transaction-rev1-btaRAandAAandTTPv1-role-1.bpel"));
+    }
 
 }
