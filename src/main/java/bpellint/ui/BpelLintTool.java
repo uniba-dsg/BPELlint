b6b7ce8a7654f3a3096554abaf22e255dc428725
Simon Harrer <simon.harrer@gmail.com>
2015-01-09 15:55:55 +0100

    Use varargs instead of array to allow for easier calling the main method.

--- src/main/java/bpellint/ui/BpelLintTool.java
+++ src/main/java/bpellint/ui/BpelLintTool.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 
 public class BpelLintTool {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         try {
             CommandLineInterpreter input = new CommandLineInterpreter();
             CLIOptions options = input.parse(args);
